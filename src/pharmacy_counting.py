2a2cdd3d8643151d91a017c188b4493c249b4f46
kudiyar <kudi.kz@gmail.com>
2019-01-31 21:59:28 -0500

    Add files via upload

--- src/pharmacy_counting.py
+++ src/pharmacy_counting.py
@@ -8,33 +8,29 @@ Created on Tue Jan 29 13:19:36 2019
 import sys
 # filename
 input_file = str(sys.argv[1])
-#input_file = r'C:\Users\Asus\Documents\GitHub\pharmacy_count\itcont.txt'
-# reading the rest of the file
-drugs = {}
+dictionary = {}                
 with open(input_file) as file:
     headers = next(file).strip('\n').split(',')
     # variable indices
-    prescriber_id = headers.index('id')
-    drug_name = headers.index('drug_name')
-    drug_cost = headers.index('drug_cost')
-    # reading the data
+    prescriber_id_idx = headers.index('id')
+    drug_name_idx = -2
+    drug_cost_idx = -1
     for line in file:
         line = line.strip('\n').split(',')
-        # line = [line[prescriber_id], line[drug_name], line[drug_cost]]
-        #prescriber_id = int(line[prescriber_id])
-        if line[drug_name] in drugs:
-            if line[prescriber_id] not in drugs[line[drug_name]][0]:
-                drugs[line[drug_name]][0].append(line[prescriber_id])
-            drugs[line[drug_name]][1] += int(line[drug_cost])                
+        sample_drug = line[drug_name_idx].strip('"')
+        name = (line[1] + ' ' + line[-3]).split(' ')
+        name = name[0] + ' ' + name[-1]
+        if sample_drug in dictionary.keys():
+            dictionary[sample_drug][1] += float(line[drug_cost_idx])
+            dictionary[sample_drug][0].add(name)
         else:
-            drugs[line[drug_name]] = [[line[prescriber_id]], int(line[drug_cost])]
-      
-output_data = sorted(drugs.items(), key=lambda x: (-x[1][1], x[0]))    
-#output_file = r'C:\Users\Asus\Documents\GitHub\pharmacy_count\top_cost_drug.txt'
-#with open(output_file,"w") as file1:
+            dictionary[sample_drug] = [{name}, 
+                       float(line[drug_cost_idx])]
+
+output_data = sorted(dictionary.items(), key=lambda x: (-x[1][1], x[0]))    
 file1 = open(str(sys.argv[2]), "w")
 output = ["drug_name,num_prescriber,total_cost\n"]
 output += [output_line[0] + ',' + str(len(output_line[1][0])) + ',' + 
-           str(output_line[1][1]) + '\n' for output_line in output_data]     
+           str(output_line[1][1]) + "\n" for output_line in output_data]     
 file1.writelines(output)
 file1.close()
